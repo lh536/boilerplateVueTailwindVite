09c5e12075f57753526d5e238f37202c5065a6b0
Fabio Duarte <mail.fabio.duarte@gmail.com>
2022-05-22 21:07:08 -0500

    chore: 'vue@3.2.35', 'vue-router@4.0.15', 'vuex@4.0.2', 'vite@2.9.9' and 'tailwindcss@3.0.24'

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { resolve } from 'path'
+import { fileURLToPath, URL } from 'url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-    },
-  },
-
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
